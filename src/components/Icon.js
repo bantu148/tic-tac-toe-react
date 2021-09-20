88bd3ae3a613b6e79cbc57142131dbd655af146a
bantu148 <58593495+bantu148@users.noreply.github.com>
2021-09-20 22:16:18 +0530

    react tic tac toe

---
+++ src/components/Icon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaTimes, FaPen, FaRegCircle } from "react-icons/fa";
+
+const Icon = ({ name }) => {
+  console.log(name);
+  switch (name) {
+    case "Circle":
+      return <FaRegCircle className="icons" />;
+    case "Cross":
+      return <FaTimes className="icons" />;
+    default:
+      return <FaPen className="icons" />;
+  }
+};
+
+export default Icon;
